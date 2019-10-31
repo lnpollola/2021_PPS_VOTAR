21c7711d87a9e6c42a42ea9f7b4883b603e3d84d
Julian Graziano <jngraziano@gmail.com>
2019-10-31 11:21:31 -0300

    githubIO

--- src/app/componentes/header/header.component.ts
+++ src/app/componentes/header/header.component.ts
@@ -39,7 +39,7 @@ export class HeaderComponent implements OnInit {
   }
 
   openLoginForm(){
-    this.dialog.open(LoginComponent, {width:'100%', height:'100%'});
+    this.dialog.open(LoginComponent, {width:'100%', height:'100%', disableClose: true});
    
   }
 
