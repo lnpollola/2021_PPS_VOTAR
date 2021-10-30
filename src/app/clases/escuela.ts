88700b62b4d05ecb041463f48527a443665d0af7
Leandro Pollola <leandro.pollola@xpo.com>
2021-10-30 00:21:52 -0300

    Gestion escuelas funcionando OK.Listado y Agregar / Eliminar

--- src/app/clases/escuela.ts
+++ src/app/clases/escuela.ts
@@ -1,6 +1,16 @@
 export class Escuela {
-    id:string;
-    direccion:string;
-    distrito:string;
-    nombre:string;
+    public idEscuela: number;
+    public direccion: string;
+    public distrito: string;
+    public nombre: string;
+    public estado: string;
+
+    constructor(nroEscuela:number, direccion:string, distrito:string, nombre:string , estado:string ) {
+        this.idEscuela = nroEscuela;
+        this.direccion = direccion;
+        this.distrito = distrito; 
+        this.nombre = nombre;
+        this.estado = estado;
+      }
+
 }
--- src/app/componentes/gestion-escuelas/gestion-escuelas.component.ts
+++ src/app/componentes/gestion-escuelas/gestion-escuelas.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseService } from '../../servicios/firebase.service';
+import * as firebase from "firebase";
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Escuela } from 'src/app/clases/escuela';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-gestion-escuelas',
@@ -7,112 +13,172 @@ import { FirebaseService } from '../../servicios/firebase.service';
   styleUrls: ['./gestion-escuelas.component.css']
 })
 export class GestionEscuelasComponent implements OnInit {
-
-  spinner:boolean;
-  listaPendientesCocina: Array<any>;
-  listaPendientesChopera: Array<any>;
-  listaPendientesCandy: Array<any>;
-  listaPendientesBarra: Array<any>;
-  listaPendientes: Array<any>;
-  usuarioLogeado: any;
-
-  listaPedidos: Array<any>;
-  // tiempoPreparacion:number;
   
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'preparacion', 'acciones'];
+  listaMesas;
+  listaEscuelas;
+  importe;
+  display: boolean = false;
 
-  constructor(private baseService:FirebaseService) {
+  noIMGcargada: boolean = false;
+
+  listaUsuarios:Array<any>;
+  usuarioRegistrado: boolean = false;
+  agregOK:boolean = false;
+  eliminOK:boolean = false;
+  perfiles = [
+  ];
+
+  imagenNueva: any;
+  checkagregoimagen: boolean = false;
+  agregoimagenErrorMsg: boolean = false;
+  num = new FormControl('', [
+    Validators.required,
+  ]);
+  
+  direccion = new FormControl('', [ ]);
+  distrito = new FormControl('', [ ]);
+  nombre = new FormControl('', [ ]);
+  perfil = new FormControl(null, [ ]);
+  
+
+  registroForm: FormGroup = this.builder.group({
+    num: this.num,
+    direccion: this.direccion,
+    distrito: this.distrito,
+    nombre: this.nombre
+  });
+
+  constructor( private builder: FormBuilder,
+    private baseService:FirebaseService) {
+    this.perfil=   JSON.parse(sessionStorage.getItem('Usuarios')).perfil;
+  
+    this.TraerLasEscuelas();
    }
-
-  TraerTabla()
-  {
-    this.spinner=true;
-    // this.baseService.getItems("comanda/Pedidos").then(pedidos => {
-      this.baseService.getItems("comanda/PedidosDetalle").then(pedidosDetalle => {
-
-      this.listaPendientesCocina = [];
-      this.listaPendientesChopera = [];
-      this.listaPendientesCandy = [];
-      this.listaPendientesBarra = [];
-
-      this.listaPedidos = [];
-
-      // this.listaPedidos= pedidos;
-      let listaPedidosDetalle = pedidosDetalle;
-     
-
-    listaPedidosDetalle.forEach(element => {
-
-                 element = {
-                idPedido: element.idPedido,
-                id: element.id, 
-                estado: element.estado,
-                nombre: element.nombre,
-                precio: element.precio,
-                sector: element.sector,
-                }
-    
-    if (this.usuarioLogeado.perfil == "mozo") {
-
-            this.listaPedidos.push(element);
-            this.listaPendientes = this.listaPedidos;
    
-          }
+  TraerLasEscuelas() {
+    this.baseService.getItems("votar/Escuelas").then(escuelas => {
       
-      switch (element.sector) {
-        case "cocina":
-        this.listaPendientesCocina.push(element);
-        if (this.usuarioLogeado.perfil == "cocina") {
-          this.listaPendientes = this.listaPendientesCocina;
-          
-        }
-        break;
-        case "chopera":
-        this.listaPendientesChopera.push(element);
-        if (this.usuarioLogeado.perfil == "chopera") {
-          this.listaPendientes = this.listaPendientesChopera;
-          
-        }
-        break;
-        case "candy":
-        this.listaPendientesCandy.push(element);
-        if (this.usuarioLogeado.perfil == "candy") {
-          this.listaPendientes = this.listaPendientesCandy;
-          
-        }
-        break;
-        case "barra":
-        this.listaPendientesBarra.push(element);
-        if (this.usuarioLogeado.perfil == "barra") {
-          this.listaPendientes = this.listaPendientesBarra;
-          
-        }
-        break;
+      this.listaEscuelas = escuelas;
+      // console.log(this.listaEscuelas[0].nombre);
+      var long = [];
+      
+      for (let index = 0; index < this.listaEscuelas.length; index++) {
+        this.perfiles[index] = this.listaEscuelas[index].nombre;
+        
+      }
+     
+      console.log(this.perfiles);
     
-        default:
-           
-            break;
-        }
-
-          
-       });
-
-       
-  }); //FIN PEDIDODETALLE
-
-// }); // FIN PEDIDOS
-          
-  
-
-
+    });
   }
- ngOnInit() {
-   this.usuarioLogeado = JSON.parse(sessionStorage.getItem("Usuarios"));
 
-   this.TraerTabla();
-
-
- }
+  ngOnInit() {
+  }
 
 
+  borrarEscuela(Escuela){
+    console.log(Escuela);
+    this.baseService.removeItem('votar/Escuelas', Escuela.key );
+
+    this.agregOK = false;
+    this.eliminOK = true;
+    this.TraerLasEscuelas();
+  }
+
+  IngresarEscuela()
+   {
+            let escuelaNuev = new Escuela(this.registroForm.get('num').value,
+                                          this.registroForm.get('direccion').value,
+                                          this.registroForm.get('distrito').value,
+                                          this.registroForm.get('nombre').value,
+                                          "cerrada"
+                                          );
+           
+            this.baseService.addItem('votar/Escuelas', escuelaNuev); 
+            this.eliminOK = false;
+            this.agregOK = true;
+
+            this.registroForm.reset();
+                
+          this.TraerLasEscuelas();
+        // }
+      }
+
+  
+      descarga(){
+        this.eliminOK = false;
+        const documentDefinition = { content: [
+            {
+                text: 'Listado de Escuelas',
+                bold: true,
+                fontSize: 20,
+                alignment: 'center',
+                decoration: 'underline',
+                margin: [0, 0, 0, 20]
+            },
+            this.getListaEscuelasPDF(),
+        
+          ],
+              styles: {
+                name: {
+                  fontSize: 14,
+                },
+                jobTitle: {
+                  fontSize: 16,
+                  bold: true,
+                  italics: true
+                }
+              }
+            }
+    
+        
+        pdfMake.createPdf(documentDefinition).download('ListadoEscuelas.pdf');
+      
+      }
+      getListaEscuelasPDF(){
+        const exs = [];
+        this.listaEscuelas.forEach(element => {
+          exs.push(
+            [{
+              columns: [
+                [{
+                  text: "Nro Escuela: "+ element.idEscuela,
+                  style: 'jobTitle'
+                },
+                {
+                  text:  "Nombre: "+ element.nombre,
+                  style: 'name'
+                },
+                {
+                  text:  "Direccion: "+ element.direccion,
+                  style: 'name'
+                },
+                {
+                  text:  "Localidad: "+ element.distrito,
+                  style: 'name'
+                },
+                {
+                  text:  "Estado: "+ element.estado,
+                  style: 'name'
+                },
+                {
+                  text:  "Firebase Key: "+ element.key,
+                  style: 'name'
+                },
+              ]
+              ]
+            }]
+          );
+        });
+        return {
+          table: {
+            widths: ['*'],
+            body: [
+              ...exs
+            ]
+          }
+        };
+      
+      }
+    
 }
