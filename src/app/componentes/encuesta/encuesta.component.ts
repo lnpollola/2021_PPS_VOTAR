8be9ebe1b2840f412e9cdcf4afc21e364bfa5a1f
Leandro Pollola <leandro.pollola@xpo.com>
2021-11-09 22:02:37 -0300

    Commit OK guardo currentRateCoci

--- src/app/componentes/encuesta/encuesta.component.ts
+++ src/app/componentes/encuesta/encuesta.component.ts
@@ -66,10 +66,10 @@ export class EncuestaComponent implements OnInit {
 
   createForm() {
     this.feedbackForm = this.fb.group({
-      firstname: ['',[Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
-      lastname: ['',[Validators.required, Validators.minLength(2), Validators.maxLength(25)]],
-      telnum: [0 ,[Validators.required, Validators.pattern]],
-      email: ['',[Validators.required,Validators.email]],
+      firstname: [''],
+      lastname: [''],
+      telnum: [this.currentRateCoci],
+      email: [''],
       agree: false,
       contacttype: 'None',
       message: ''
@@ -82,31 +82,44 @@ export class EncuestaComponent implements OnInit {
   }
 
   onValueChanged(data?: any) {
-    if (!this.feedbackForm) { return; }
-    const form = this.feedbackForm;
-    
-    for (const field in this.formErrors) {
-      if (this.formErrors.hasOwnProperty(field)) {
-        // clear previous error message (if any)
-        this.formErrors[field] = '';
-        const control = form.get(field);
-        if (control && control.dirty && !control.valid) {
-          const messages = this.validationMessages[field];
-          for (const key in control.errors) {
-            if (control.errors.hasOwnProperty(key)) {
-              this.formErrors[field] += messages[key] + ' ';
-            }
-          }
-        }
-      }
-    }
+    // if (!this.feedbackForm) { return; }
+   
+
+      this.feedbackForm = this.fb.group({
+        firstname: [''],
+        lastname: [''],
+        telnum: [this.currentRateCoci],
+        email: [''],
+        agree: false,
+        contacttype: 'None',
+        message: '',
+        currentRateCoci: ''
+      });
+
+      const form = this.feedbackForm;
+    return;
+    // for (const field in this.formErrors) {
+    //   if (this.formErrors.hasOwnProperty(field)) {
+    //     // clear previous error message (if any)
+    //     this.formErrors[field] = '';
+    //     const control = form.get(field);
+    //     if (control && control.dirty && !control.valid) {
+    //       const messages = this.validationMessages[field];
+    //       for (const key in control.errors) {
+    //         if (control.errors.hasOwnProperty(key)) {
+    //           this.formErrors[field] += messages[key] + ' ';
+    //         }
+    //       }
+    //     }
+    //   }
+    // }
   }
 
   onSubmit() {
     this.feedback = this.feedbackForm.value;
     console.log(this.feedback);
 
-    this.baseService.addItem('comanda/Encuestas',this.feedback); 
+    this.baseService.addItem('votar/Encuestas',this.feedback); 
 
 
     this.feedbackForm.reset({
